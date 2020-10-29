a772e4eb92dfe5a0ee8bd269f125f7dbd80f983f
pissulin <pissulin.ads@gmail.com>
2020-10-28 23:18:55 -0300

    correção da taxa para float

--- src/components/RowHistorico.js
+++ src/components/RowHistorico.js
@@ -3,7 +3,7 @@ import './RowHistorico.css'
 
 function Row(props) {
     const num = props.numero
-    const taxa = parseInt(props.taxa)
+    const taxa = parseFloat(props.taxa)
     return (
         <div className="row">
             <h2 id="comanda">Nº Comanda: { num in [1,2,3,4,5,6,7,8,9,10]? '0'+ props.numero: props.numero}</h2>
