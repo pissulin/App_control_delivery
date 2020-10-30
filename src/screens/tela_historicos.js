a4910acea030b2f27046dad7c6acf60682e3ff09
pissulin <pissulin.ads@gmail.com>
2020-10-30 00:20:13 -0300

    mensagem quando nao tem nada salvo no historico

--- src/screens/tela_historicos.js
+++ src/screens/tela_historicos.js
@@ -4,41 +4,64 @@ import {Link} from 'react-router-dom'
 import {FiArrowLeftCircle, FiArrowRightCircle, FiRefreshCcw} from 'react-icons/fi'
 
 import Historicos from '../components/ContainerHistorico'
-import './tela_historico.css'
+import './tela_historicos.css'
 
 
 
 function HistoricosSalvos(){
+    try {
+        const arrayHistorico = require('../db/db_historico.json')
 
-    const arrayHistorico = require('../db/db_historico.json')
+        const data = arrayHistorico.entregas
 
-    const data = arrayHistorico.entregas
+        console.log(data)
 
-    console.log(data)
-
-    return (
-        <div className="container">
-          {data.map((entrega,index) => {
-                return <Historicos 
-                            data={entrega['data']} 
-                            totalEntregas={entrega['totalEntregas']}
-                            totalTaxas={entrega['totalTaxas']}
-                            diaria={entrega['diaria']}
-                            totalReceber={entrega['totalReceber']}
-                         />
-            })}  
-            <div className="setas">
-                <Link to='/fechamento'>
-                    <FiArrowLeftCircle className='setaDireita' size={50} color="white" />
-                </Link>
-                <Link to='/'>
-                    <FiArrowRightCircle className='setaDireita' size={50} color="white" />
-                </Link>
+        return (
+            <div className="container">
+            {data.map((entrega) => {
+                    return <Historicos 
+                                data={entrega['data']} 
+                                totalEntregas={entrega['totalEntregas']}
+                                totalTaxas={entrega['totalTaxas']}
+                                diaria={entrega['diaria']}
+                                totalReceber={entrega['totalReceber']}
+                            />
+                })}  
+                <div className="setas">
+                    <Link to='/fechamento'>
+                        <FiArrowLeftCircle className='setaDireita' size={50} color="white" />
+                    </Link>
+                    <Link to='/'>
+                        <FiArrowRightCircle className='setaDireita' size={50} color="white" />
+                    </Link>
+                </div>
             </div>
-        </div>
-        
-    )
+            
+        )
     
-}
+    } catch (error) {
+        
+            return (
+                <div className="container">
+                    <div className="mensagem">
+                        <h1>Você não tem histórico salvo</h1>
+                    </div>
+                    
+                    <div className="setas">
+                        <Link to='/fechamento'>
+                            <FiArrowLeftCircle className='setaDireita' size={50} color="white" />
+                        </Link>
+                        <Link to='/'>
+                            <FiArrowRightCircle className='setaDireita' size={50} color="white" />
+                        </Link>
+                    </div>
+                </div>
+                
+            )
+            
+        }
+    }
+
+    
 
 export default HistoricosSalvos
