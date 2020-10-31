bc6bb49eea7ec26f4f2d82430b666efd1e4ff766
pissulin <pissulin.ads@gmail.com>
2020-10-31 15:24:07 -0300

    feature input caixinha e confirmao de exclusao

--- src/components/RowHistorico.js
+++ src/components/RowHistorico.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './RowHistorico.css'
 
 function Row(props) {
-    const num = props.numero
-    const taxa = parseFloat(props.taxa)
+    const num = props.numero || 0
+    const taxa = parseFloat(props.taxa) || 0
+    const caixinha = parseFloat(props.caixinha) || 0
     return (
         <div className="row">
-            <h2 id="comanda">Nº Comanda: { num in [1,2,3,4,5,6,7,8,9,10]? '0'+ props.numero: props.numero}</h2>
-            <h2>|</h2>
-            <h2 id ="taxa">Taxa: R$ {taxa.toFixed(2)}</h2>
+            <h2 id="comanda">Comanda: { num in [1,2,3,4,5,6,7,8,9,10]? '0'+ props.numero: props.numero}</h2>
+            <h2><span>|</span></h2>
+            <h2 id ="taxa">Taxa: R$ {taxa < 10? '0' + taxa.toFixed(2):  taxa.toFixed(2)}</h2>
+            <h2><span>|</span></h2>
+            <h2 id ="taxa">Caixinha: R$ {caixinha < 10? '0' + caixinha.toFixed(2): caixinha.toFixed(2)}</h2>
         </div>
     )
 }
