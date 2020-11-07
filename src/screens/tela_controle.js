36a1274554fb5e59350486dffc17efc2413218ad
pissulin <pissulin.ads@gmail.com>
2020-11-07 16:12:45 -0300

    time correção

--- src/screens/tela_controle.js
+++ src/screens/tela_controle.js
@@ -11,7 +11,7 @@ let id = 0;
 
 function Controle () {
     const data = new Date()
-    const dataNormal =  new Date(data.valueOf() - data.getTimezoneOffset() * 120000)
+    const dataNormal =  new Date(data.valueOf() - data.getTimezoneOffset() * 60000)
     const dataAtual = `${data.getDate().toLocaleString('pt-BR',{timeZone:'America/Sao_Paulo'})}/${(data.getMonth()+1)}/${data.getFullYear()} - ${data.getHours()}:${data.getMinutes()}` 
 
     const [numComanda, setNumComanda] = useState('')
