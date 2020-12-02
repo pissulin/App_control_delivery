edb356089540cbea14da4e02f0ac3768afeeeac5
pissulin <pissulin.ads@gmail.com>
2020-12-02 19:22:39 -0300

    inputs do formulario

--- src/screens/tela_cadastro.js
+++ src/screens/tela_cadastro.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import {React, useState} from 'react'
+import {Link} from 'react-router-dom'
 
 import './tela_cadastro.css'
 
 function Cadastro(){
+
+    const [nomePizzaria, setNomePizzaria] = useState('')
+
+
     return (
         <form className="form" onSubmit={()=> {return}} autoComplete={"false"}>
-            <h1>Formulario aqui</h1>
+            <label htmlFor= "NomePizzaria">Noma da pizzaria</label>
+                <input 
+                    id="nomePizzaria" 
+                    value={nomePizzaria} 
+                    onChange={event => setPizzaria(event.target.value)}
+                    maxLength={10}
+                    autoComplete={false}
+                    autoFocus={true}
+                    inputMode="text" 
+                />
+
+            <label htmlFor= "NomePizzaria">Noma da pizzaria</label>
+                <input 
+                    id="diaria" 
+                    value={diaria} 
+                    onChange={event => setDiaria(event.target.value)}
+                    maxLength={10}
+                    autoComplete={false}
+                    autoFocus={true}
+                    inputMode="numeric" 
+                />
+
+
+
         </form>  
     )
 }
