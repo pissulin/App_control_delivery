827e3d8af47dc4cb7f43886ae57f6434aab4c51f
pissulin <pissulin.ads@gmail.com>
2020-12-02 19:42:09 -0300

    .

--- src/screens/tela_cadastro.js
+++ src/screens/tela_cadastro.js
@@ -8,38 +8,63 @@ function Cadastro(){
     const [nomePizzaria, setNomePizzaria] = useState('')
     const [diaria, setDiaria] = useState('')
 
-    return (
-        <form className="form" onSubmit={()=> {return}} autoComplete={"false"}>
-            <label htmlFor= "NomePizzaria">Nome da pizzaria</label>
-                <input 
-                    id="nomePizzaria" 
-                    value={nomePizzaria} 
-                    onChange={event => setNomePizzaria(event.target.value)}
-                    maxLength={250}
-                    autoComplete={false}
-                    autoFocus={true}
-                    inputMode="text" 
-                />
+    function handleSubmit(event){
+        event.preventDefault()
 
-            <label htmlFor= "NomePizzaria">Diária</label>
-                <input 
-                    id="diaria" 
-                    value={diaria} 
-                    onChange={event => setDiaria(event.target.value)}
-                    maxLength={10}
-                    autoComplete={false}
-                    autoFocus={true}
-                    inputMode="numeric" 
-                />
-            <button   
-                className="submit" 
-                type="submit"
-            >
-             Salvar
-            </button>
-        </form>  
-    )
-}
+        const regexValidaInput = RegExp(/^[,.]/g)
+         
+        if(regexValidaInput.test(nomePizzaria)){
+            
+            return alert('Deu certo')
+        }
+
+        if(regexValidaInput.test(diaria) ){
+           
+            return  alert('Deu certo')
+        } 
+
+            document.getElementById('nomePizzaria').value='';
+            document.getElementById('diaria').value='';
+
+        }
+
+
+        return (
+            <form className="form" onSubmit={handleSubmit} autoComplete={"false"}>
+                <label htmlFor= "NomePizzaria">Nome da pizzaria</label>
+                    <input 
+                        id="nomePizzaria" 
+                        value={nomePizzaria} 
+                        onChange={event => setNomePizzaria(event.target.value)}
+                        maxLength={250}
+                        autoComplete={false}
+                        autoFocus={true}
+                        inputMode="text" 
+                    />
+    
+                <label htmlFor= "NomePizzaria">Diária</label>
+                    <input 
+                        id="diaria" 
+                        value={diaria} 
+                        onChange={event => setDiaria(event.target.value)}
+                        maxLength={10}
+                        autoComplete={false}
+                        autoFocus={true}
+                        inputMode="numeric" 
+                    />
+                <button   
+                    className="submit" 
+                    type="submit"
+                >
+                 Salvar
+                </button>
+            </form>  
+        )
+
+    }
+
+    
+
 
 
 export default Cadastro
