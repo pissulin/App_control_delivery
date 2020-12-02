97a454c563b560bf5bdb1f0aea9a9e0d83b25d8d
pissulin <pissulin.ads@gmail.com>
2020-12-02 18:59:08 -0300

    .

--- src/screens/tela_cadastro.js
+++ src/screens/tela_cadastro.js
@@ -4,9 +4,11 @@ import './tela_cadastro.css'
 
 function Cadastro(){
     return (
+        <>
         <form className="form" onSubmit={handleSubmit} autoComplete={"false"}>
             <h1>Formulario aqui</h1>
         </form>
+        </>
     )
 }
 
