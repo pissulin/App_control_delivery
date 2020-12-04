f90dbfe255e7600690107f1968c0c3dd822fc341
pissulin <pissulin.ads@gmail.com>
2020-12-03 21:53:33 -0300

    funções do db_localStorage okay

--- src/db/db.js
+++ src/db/db.js
@@ -8,13 +8,37 @@ const delStorage = (key) => localStorage.removeItem(key)
 const deletarItem = (estabelecimento) => {
     let db_estabelecimento = JSON.parse(getStorage('estabelecimentos'))
     for(let i = 0; i < db_estabelecimento.length; i++){
-        if(db_estabelecimento[i].estabelecimento === estabelecimento)
+        if(db_estabelecimento[i].estabelecimento === estabelecimento){
             db_estabelecimento.splice(i, 1)
             setStorage('estabelecimentos', JSON.stringify(db_estabelecimento))
         }
+            
     }
+}
+
+const updateItem = (estabelecimento, nome, diaria) => {
+    let db_estabelecimento = JSON.parse(getStorage('estabelecimentos'))
+    for(let i = 0; i < db_estabelecimento.length; i++){
+        if(db_estabelecimento[i].estabelecimento === estabelecimento){
+            if(nome !== 'undefined'){
+                db_estabelecimento[i].estabelecimento = nome
+            }
+            if(diaria !== 'undefined'){
+                db_estabelecimento[i].diaria = diaria
+            }
+            delStorage('estabelecimentos')
+            setStorage('estabelecimentos', JSON.stringify(db_estabelecimento))
+        }
+            
+    }
+}
 
 
 
-
-export default {getStorage, setStorage, delStorage, deletarItem}
+export default {
+        getStorage, 
+        setStorage, 
+        delStorage, 
+        deletarItem,
+        updateItem,
+    }
